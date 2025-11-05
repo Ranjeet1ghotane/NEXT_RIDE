f697ba12a55a629fd75d27dccb5a4f71478fd0e5
Ranjeet1ghotane <ranjeetghotane2002@gmail.com>
2025-11-05 06:59:36 +0530

    Commiting Project

--- NEXT_RIDE/NEXT_RIDE/CarRentalNETBackend/obj/Release/net8.0/CarRentalNETBackend.AssemblyInfo.cs
+++ NEXT_RIDE/NEXT_RIDE/CarRentalNETBackend/obj/Release/net8.0/CarRentalNETBackend.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalNETBackend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21471178222644b3d556337cca27a665594d678b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalNETBackend")]
